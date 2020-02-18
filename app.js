259c997c092df6443cf23eba9c8368d14d892f3a
Armadillo <ajknapp2015@gmail.com>
2020-02-17 18:12:57 -0800

    created preliminary pageMaker

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const fs = require('fs');
 const question = require('./lib/question');
+const pageMaker = requre('./lib/pageMaker');
 
-question.questions.then(result => console.log(result)).catch(err => console.log(err));
+question.questions
+    .then(
+        result => pageMaker.pageWritter(result)
+    ).catch(
+        err => console.log(err)
+    );
     
